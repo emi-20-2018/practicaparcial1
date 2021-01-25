089bfeb5cb8ca300548c59afab0d686cd8589ae6
Emily <emi.roca18@gmail.com>
2021-01-25 16:31:28 -0400

    Creacion modelo

---
+++ p.parcial1/p.parcial1/Models/ross.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace p.parcial1.Models
+{
+    public enum lista { univerdad,casa,farmacia,hospital,supermercado} 
+    public class ross
+    {[Key]
+        public int rossID { set; get; }
+        [Required] [Display(Name ="Nombre Completo")] [StringLength(24,MinimumLength =2)]
+        public string Friendofross { set; get; }
+        [Required]
+        public lista place { set; get; }
+        [EmailAddress(ErrorMessage ="ponga un correo valido")]
+        public string Email { set; get; }
+        [Display(Name ="Cumpleaños")]
+        public DateTime Birthdate { set; get; }
+
+
+    }
+}
